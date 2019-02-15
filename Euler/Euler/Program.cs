ee12cfdbb8e0b09b533d405c77a80b500e6b006c
Mawande Hobo <31037210+hobomawande@users.noreply.github.com>
2019-02-15 09:40:03 +0200

    Update Program.cs

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -7,9 +7,29 @@ using System.Threading.Tasks;
 namespace Euler
 {
     class Program
-    {
+    { public static int FindSmallestDiviser(int num)
+        {
+            int sumSquares = 0;
+            for (int i = 1; i < num+1; i++)
+            {
+                sumSquares += (i * i);
+            }
+            int sum = 0;
+            for (int xs = 1; xs < num+1; xs++)
+            {
+                sum += xs;
+            }
+            int diff = (sum * sum) - sumSquares;
+
+
+
+            return diff;
+        }
         static void Main(string[] args)
         {
+             Console.WriteLine("The number is :" + FindSmallestDiviser(100));
+           
+            Console.ReadLine();
         }
     }
 }
