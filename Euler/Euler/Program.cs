fe789975989abb59e81177a215aa5abc77a923d1
Spidz67 <31043687+Spidz67@users.noreply.github.com>
2019-02-15 09:40:08 +0200

    Update Program.cs

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -8,8 +8,41 @@ namespace Euler
 {
     class Program
     {
+        public static void findPrime()
+        {
+            int count = 1;
+            int idx = 3;
+            while( true)
+            {
+                int nums = 1;
+                bool isPrime = true;
+                for(int i  = 2; i  <= idx; i++)
+                {
+                    if(idx % i == 0)
+                    {
+                        nums++;
+                        if (nums > 2)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    
+                }
+                if (isPrime)
+                {
+                    count++;
+                    if(count == 10001)
+                    {
+                        Console.WriteLine("the 10 001st prime number is " + idx);
+                    }
+                }
+                idx++;
+            }
+        }// Finds the 10001st prime number
         static void Main(string[] args)
         {
+            findPrime();
         }
     }
 }
