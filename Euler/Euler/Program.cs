adecaa45648348719b8b50fb67a2896028cdede2
lubabalobuwa <31037217+lubabalobuwa@users.noreply.github.com>
2019-02-15 09:52:01 +0200

    Update Program.cs

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -8,8 +8,23 @@ namespace Euler
 {
     class Program
     {
+        public static void Multiples()
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("sum of all the multiples of 3 or 5 below 1000 is:");
+            Console.WriteLine(sum);
+        }
+
         static void Main(string[] args)
         {
+            Multiples();
         }
     }
 }
